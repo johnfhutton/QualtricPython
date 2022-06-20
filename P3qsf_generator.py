540554ecf7beccf4d32b17430f4745280a6f0748
JohnHutton <john.f.hutton@gmail.com>
2022-06-19 19:22:48 -0600

    Updated for Academic.  Needed minor change to replace...

--- P3qsf_generator.py
+++ P3qsf_generator.py
@@ -4,16 +4,16 @@ import re
 
 # Read the out file in the form of
 # ResponseID, Last, First, email, External, Q2_1, ..., Q2_37
-df=pd.read_csv('IndP2-Ranking_29samples.csv',keep_default_na=False)
+df=pd.read_csv('AcaP2-Ranking_24samples.csv',keep_default_na=False)
 #df.head()
 #int(df.at[1,'Q2_1'])
 dfT=df.transpose()
 [rows,cols] = dfT.shape  # Cols are now series with one respondents info.
 
-question=["Q2_1","Q2_2","Q2_3","Q2_4","Q2_5","Q2_6","Q2_7","Q2_8","Q2_9","Q2_10","Q2_11","Q2_12","Q2_13","Q2_14","Q2_15","Q2_16","Q2_17","Q2_18","Q2_19","Q2_20","Q2_21","Q2_22","Q2_23","Q2_24","Q2_25","Q2_26","Q2_27","Q2_28","Q2_29","Q2_30","Q2_31","Q2_32","Q2_33","Q2_34","Q2_35","Q2_36","Q2_37"]
-qsfNumber=[93,   101,   41,    92,    102,   99,    111,   104,   120,    95,     126,    118,    110,    115,    124,    121,    117,    103,    123,    108,    98,     109,    122,    105,    119,    94,     96,     114,    97,     91,     113,    106,    107,    125,    112,    116,    100]
+question=["Q2_12","Q2_2","Q2_4","Q2_1","Q2_24","Q2_19","Q2_30","Q2_8","Q2_22","Q2_10","Q2_11","Q2_3","Q2_6","Q2_15","Q2_13","Q2_17","Q2_21","Q2_34","Q2_35","Q2_28","Q2_16","Q2_20","Q2_29","Q2_26","Q2_18","Q2_5","Q2_23","Q2_31","Q2_9","Q2_7","Q2_14","Q2_27","Q2_25","Q2_33","Q2_32"]
+qsfNumber=[41,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124]
 
-with open('IndP3-Ranking-Default.qsf') as myfile:
+with open('AcaP3-Starter.qsf') as myfile:
     qfile = myfile.read()
 
 # Loop and output a new file which each row  (Should probably do this as a function)
@@ -57,10 +57,10 @@ for response in dfT:
                 fr'\1\2', \
                 responseFile)
     # Output this as a new qsf file
-    surveyName = "IndP3-Ranking" + str(dfT[response]["RecipientEmail"])
+    surveyName = "AcaP3-Starter" + str(dfT[response]["RecipientEmail"])
     fileName = f"{surveyName}.qsf"
     responseFile = re.sub( \
-        fr'(,"SurveyName":)".*?(",)', \
+        fr'(,"SurveyName":)".*?(","SurveyDescription":null,)', \
         fr'\1"{surveyName}\2', \
         responseFile)
         
